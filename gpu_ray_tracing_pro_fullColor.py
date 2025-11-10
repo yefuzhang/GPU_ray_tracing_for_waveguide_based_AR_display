ef4c53f63eab5f2b866c51429a7b318c891d164c
yefuzhang <zhangyefu1999@163.com>
2025-11-10 14:15:31 -0500

    Update gpu_ray_tracing_pro_fullColor.py

--- gpu_ray_tracing_pro_fullColor.py
+++ gpu_ray_tracing_pro_fullColor.py
@@ -57,7 +57,7 @@ d_lut_TIR = cuda.to_device(lut_TIR)
 d_lut_gap = cuda.to_device(lut_gap)
 
 # generating huge ray matrix
-num_iter = 6
+num_iter = 4
 num_rays_per_FoV = 5000 # the number of rays per FoV
 matrix_size = num_rays_per_FoV*num_FOV_x*num_FOV_y*len(lmd)
 num_rays = matrix_size
@@ -235,5 +235,6 @@ axes[2].set_title('Normalized FoV Efficiency B')
 plt.tight_layout()
 
 plt.show()
+
 
 
